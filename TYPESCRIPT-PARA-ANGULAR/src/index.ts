7d8b60adf8257c146479d83d5b41e593d4e02e53
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-08-04 16:30:47 -0300

    trabalhando com classes

--- TYPESCRIPT-PARA-ANGULAR/src/index.ts
+++ TYPESCRIPT-PARA-ANGULAR/src/index.ts
@@ -1,46 +1,19 @@
-// interfaces (type x interface)
+// classes -> forma / molde
+class Character {
+    name: string
+    strength: number
+    skill: number
 
-// type -> tipar uma variável específica - objetos
+    constructor(name: string, strength: number, skill:number) {
+        this.name = name;
+        this.strength = strength;
+        this.skill = skill;
+    }
 
-type robot = {
-    readonly id: number | string;
-    name: string;
-}
-
-const robot: robot = {
-    id: 1,
-    name: 'megaman'
-};
-
-// interfaces -> Mais usados com classes - contrato
-
-interface robot2 {
-    readonly id: number | string;
-    name: string;
-    sayhello(): string;
-};
-
-const bot: robot2 = {
-    id: 2,
-    name: 'tugao',
-    sayhello: function (): string {
-        throw new Error("Function not implemented.");
+    attack(): void {
+        console.log(`Attack with ${this.strength} points`)
     }
 }
 
-class Pessoa implements robot2 {
-    id: string | number;
-    name: string;
-
-    constructor (id: string | number, name: string) {
-        this.id = id;
-        this.name = name
-    }
-    sayhello(): string {
-        return `Hello, I am ${this.name}`;
-    }
-    
-}
-
-const p = new Pessoa(1, 'Gutsman');
-console.log(p.sayhello());
+const p1 = new Character('Ryu', 10, 98);
+p1.attack();

fddb8bc4a681984d6b317ebd659baf63c8426f4a
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-08-05 10:38:53 -0300

    subclasses

--- TYPESCRIPT-PARA-ANGULAR/src/index.ts
+++ TYPESCRIPT-PARA-ANGULAR/src/index.ts
@@ -7,8 +7,10 @@
     protected -> classes e subclasses que herdem suas propriedades
 */
 
+
+// Character -> superclass / pai
 class Character {
-    private name?: string
+    protected name?: string
     strength: number
     skill: number
 
@@ -19,9 +21,24 @@ class Character {
     }
 
     attack(): void {
-        console.log(`Attack with ${this.strength} points`)
+        console.log(`Attack with ${this.strength} points`);
+    }
+}
+
+// Magician -> subclass / Filha
+class Magician extends Character {
+    magicPoints: number;
+
+    constructor(
+        name: string, 
+        strength: number, 
+        skill: number, 
+        magicPoints: number
+    ) {
+        super(name, strength, skill);
+        this.magicPoints = magicPoints;
     }
 }
 
 const p1 = new Character('Ryu', 10, 98);
-p1.attack();
+const p2 = new Magician('Wizard', 9, 30, 99);

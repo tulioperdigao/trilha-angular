a51771212dab67f0544a222d1129c9a52eab8d48
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-08-18 16:47:34 -0300

    diretivas de atributo 1

---
+++ DIRETIVAS/diretivas-proj/src/app/comp-atributos/comp-atributos.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-atributos',
+  templateUrl: './comp-atributos.component.html',
+  styleUrls: ['./comp-atributos.component.css']
+})
+export class CompAtributosComponent implements OnInit {
+  estilo:string = "enable";
+  corFundo:string = 'blue';
+  corDaFonte:string = 'yellow';
+  item:string = '';
+  lista:string[] = [];
+  constructor() { }
+
+  trocar() {
+    if (this.estilo === 'disable') {
+      this.estilo = 'enable';
+    } else {
+      this.estilo = 'disable';
+    }
+  }
+
+  adicionarLista() {
+    this.lista.push(this.item)
+  }
+
+  ngOnInit(): void {
+  }
+
+}

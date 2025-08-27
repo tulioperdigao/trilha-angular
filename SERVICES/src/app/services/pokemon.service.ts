fc119469170e3135e162289260dcbecd94060d32
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-08-27 16:10:47 -0300

    Add HttpClient for Pokemon API requests
    Imported HttpClientModule in app.module.ts and updated PokemonService to use HttpClient for fetching Pokemon data from the API. The base URL is now set from environment variables.

--- SERVICES/src/app/services/pokemon.service.ts
+++ SERVICES/src/app/services/pokemon.service.ts
@@ -1,13 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
+  private baseUrl:string = ''
+  private pokeData:any
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+    this.baseUrl = environment.pokeApi
+   }
 
   getPokemon(pokemonName:string) {
-    console.log(pokemonName);
+    this.pokeData = this.http.get(`${this.baseUrl}${pokemonName}`);
+    console.log(this.pokeData)
   }
 }

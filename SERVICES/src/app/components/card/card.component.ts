94329baac400fde2c9fd83d303a514457f4d8825
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-08-27 15:05:30 -0300

    Make card component dynamic with name and types
    Replaced hardcoded title and types in the card component with dynamic properties. The card now displays the 'name' and iterates over 'attributesTypes' for stats, improving reusability and flexibility. Updated styles for better visibility.

--- SERVICES/src/app/components/card/card.component.ts
+++ SERVICES/src/app/components/card/card.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  name:string = 'BULBASSAUR';
+  attributesTypes:string[] = ['FIRE', 'POISON'];
+
 
   constructor() { }
 

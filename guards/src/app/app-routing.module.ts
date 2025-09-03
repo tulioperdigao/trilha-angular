16442bb0261b4a248e6ac52e7c2aa82cd3ef7044
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-09-03 16:29:54 -0300

    Update app-routing.module.ts

--- guards/src/app/app-routing.module.ts
+++ guards/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'sobre',
+    component: SobreComponent
+  },
+  {
+    path: 'produtos',
+    component: ProdutosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
